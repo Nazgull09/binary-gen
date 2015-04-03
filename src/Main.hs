a3612e7c6b98a14a5d4139a3a321bc54ab07f46d
Anatoly <nazgull09@gmail.com>
2015-04-03 06:10:43 -0700

    12

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,28 @@
 module Main where
 
 import Genes;
+import Data.List
+import Data.List.Split
+import Data.Maybe
 
 fit::Fitness
 fit chr = sum $ map fromIntegral chr
+
+nextElem :: Eq a => a -> [a] -> Maybe a         --Хаха, оказалось проще зипануть лист и его тэйл
+nextElem e xs = listToMaybe . drop 1 . dropWhile (/= e) $ xs ++ (take 1 xs)
+
+cityfit:: Int -> String -> Chromosome-> Int
+cityfit cityNum citymap chr = do sum ( map read mapfit :: [Int])
+                 where mapfit = map (\city -> (lcitymap !! (fst city)) !! (snd city) ) roadList
+                       roadList = zip decChr $ tail decChr
+                       decChr = map bin2dec $ splitEvery chrLength chr
+                       lcitymap = map (\x -> words x) $ lines citymap
+                       chrLength = ceiling $ logBase 2.0 $ fromIntegral cityNum
+                       
+
+
+bin2dec :: [Int] -> Int
+bin2dec l = sum $ map (2^) $ findIndices (==1) $ reverse l
 
 opts::EvOptions
 opts= EvOptions{populationSize = 42,
@@ -18,12 +37,23 @@ main::IO()
 main = do
    cnum <- getLine
    let cityNum = read cnum :: Int
+
    let chrLength = ceiling $ logBase 2.0 $ fromIntegral cityNum
-   let cityOpts = EvOptions 42 (chrLength*cityNum) 100 fit 10 0.3 0.1
-       
+
+   citymap <- readFile "citymap.txt"
+   let cityOpts = EvOptions 42 (chrLength*cityNum) 100 fit 10 0.3 0.1   
    (n, result) <- initEvol cityOpts
-   cityMap <- readFile "citymap.txt"
-   print cityMap
-   print $ show n ++ "-" ++ show result
-    
    
+   let lcitymap = map (\x -> words x) $ lines citymap
+   let z = [1,0,0,0,1,1] :: [Int]
+   let decChr = map bin2dec $ splitEvery chrLength z
+   let roadList = zip decChr $ tail decChr 
+   let mapfit = map (\city -> (lcitymap !! (fst city) !! (snd city) ) roadList
+--   let cityfitness = sum ( map read mapfit :: [Int])
+   let l = cityfit cityNum citymap
+   print decChr
+   print lcitymap 
+   print roadList
+   print $ l z
+   
+   --print $ show n ++ "-" ++ show result   
