29c241443d3c36067da35c82d0a106921ca0eae2
Anatoly <nazgull09@gmail.com>
2015-04-03 10:58:58 -0700

    for 4

--- src/Main.hs
+++ src/Main.hs
@@ -3,22 +3,22 @@ module Main where
 import Genes;
 import Data.List
 import Data.List.Split
+import Data.Function(on)
 import Data.Maybe
 
 fit::Fitness
 fit chr = sum $ map fromIntegral chr
 
-nextElem :: Eq a => a -> [a] -> Maybe a         --Хаха, оказалось проще зипануть лист и его тэйл
-nextElem e xs = listToMaybe . drop 1 . dropWhile (/= e) $ xs ++ (take 1 xs)
-
-cityfit:: Int -> String -> Chromosome-> Int
-cityfit cityNum citymap chr = do sum ( map read mapfit :: [Int])
+cityfit:: Int -> String -> Fitness
+cityfit cityNum citymap chr = do ((sum ( map read mapfit :: [Double])) + allq::Double)
                  where mapfit = map (\city -> (lcitymap !! (fst city)) !! (snd city) ) roadList
                        roadList = zip decChr $ tail decChr
                        decChr = map bin2dec $ splitEvery chrLength chr
                        lcitymap = map (\x -> words x) $ lines citymap
-                       chrLength = ceiling $ logBase 2.0 $ fromIntegral cityNum     
-
+                       chrLength = ceiling $ logBase 2.0 $ fromIntegral cityNum  
+                       allq = if ((length decChr) == (length $ nub decChr)) 
+                              then 0.0
+                              else -1000.0
 
 bin2dec :: [Int] -> Int
 bin2dec l = sum $ map (2^) $ findIndices (==1) $ reverse l
@@ -26,9 +26,9 @@ bin2dec l = sum $ map (2^) $ findIndices (==1) $ reverse l
 opts::EvOptions
 opts= EvOptions{populationSize = 42,
                 individLength = 10,
-                maxGeneration = 10,
+                maxGeneration = 100,
                 fitness = fit,
-                targetFitness = 10,
+                targetFitness = 0,
                 mutationChance = 0.3,
                 elitePart = 0.1}
 
@@ -37,14 +37,20 @@ main = do
    cnum <- getLine
    let cityNum = read cnum :: Int
    let chrLength = ceiling $ logBase 2.0 $ fromIntegral cityNum
-   citymap <- readFile "citymap.txt"
-   let cityOpts = EvOptions 42 (chrLength*cityNum) 100 fit 10 0.3 0.1   
-   (n, result) <- initEvol cityOpts
+   citymap <- readFile "citymap.txt"   
+
    let lcitymap = map (\x -> words x) $ lines citymap
    let z = [1,0,0,0,1,1] :: [Int]
-   let decChr = map bin2dec $ splitEvery chrLength z
-   let roadList = zip decChr $ tail decChr 
-   let mapfit = map (\city -> (lcitymap !! (fst city) !! (snd city) )) roadList
---   let cityfitness = sum ( map read mapfit :: [Int])
+   
    let l = cityfit cityNum citymap
-   print $ l z
+
+   let cityOpts = EvOptions 42 (chrLength*cityNum) 100 l 0 0.3 0.1   
+   (n, result) <- initEvol cityOpts
+  
+   print $ show n ++ "-" ++ show result 
+   print $ snd . maximumBy (compare `on` fst) $ zip (map l result) result
+   let bestChr = snd . maximumBy (compare `on` fst) $ zip (map l result) result
+   let decChr = map bin2dec $ splitEvery chrLength bestChr
+   let roadList = zip decChr $ tail decChr 
+   print roadList
+   print $ l bestChr
