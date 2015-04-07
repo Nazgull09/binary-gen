1a504092c7edd623a4414f420c01b7d8f0892edf
Anatoly <nazgull09@gmail.com>
2015-04-07 13:28:24 -0700

    working

--- src/Genes.hs
+++ src/Genes.hs
@@ -4,7 +4,6 @@ module Genes where
 import Control.Monad.Random;
 import Control.Monad;  
 import Data.Functor;
-import Data.Ord;
 import Data.Function(on)
 import Data.List(sortBy);
 import Data.Foldable(maximumBy);
@@ -26,7 +25,7 @@ data EvOptions = EvOptions{
 type Fitness = Chromosome -> Double
 
 randChoice :: Rational -> GenRand a -> GenRand a -> GenRand a
-randChoice chance th els = join (fromList [(th, chance), (els, 1 - chance)])
+randChoice chance th els = join (fromList [(th, chance), (els, 1 - chance)])        --fromList - часть рандома, выдает рандом из списка по вероятности
 
 nextPopulation::EvOptions->Population->GenRand Population
 nextPopulation EvOptions {..} pop = do
@@ -39,16 +38,19 @@ nextPopulation EvOptions {..} pop = do
             return [a3, b3]
         let newPop = elite ++ newPop'
         return $ if length newPop <= length pop then newPop else tail newPop
-        where   fits = toRational <$> fitness  <$> pop
+        where   fits = toRational <$> fitness  <$> pop                              --map fitness pop
                 maxfit = maximum fits
                 chances = zip pop ((/maxfit) <$> fits)
                 takeChr = fromList chances
-                applyMutation c = randChoice (toRational mutationChance) (mutateChromosome c) (return c)
-                sortedPop = sortBy (comparing fitness) pop
+                -- с некоторым шансом либо смутировать, либо просто венуть хромосому с
+                applyMutation c = randChoice (toRational mutationChance) (mutateChromosome c) (return c) 
+                sortedPop = snd $ unzip $ sortBy (compare `on` fst) $ zip (fitness <$> pop) pop 
+                -- другой подход к сортировке, иквабл для хромосомы
                 elite = take (ceiling $ fromIntegral (length pop) * elitePart) sortedPop
                 nonElite = length pop - length elite
 
 randChromosome::Int->GenRand Chromosome
+
 randChromosome n = replicateM n randBool
         where randBool = uniform [0, 1]
         
@@ -56,7 +58,7 @@ randPopulation::Int->Int->GenRand Population
 randPopulation n m = replicateM n $ randChromosome m
 
 mutateChromosome::Chromosome->GenRand Chromosome
-mutateChromosome chr = do 
+mutateChromosome chr = do                                --заменяем н-ый элемент инверсией
         n <- uniform [0..length chr - 1]
         return $ invertAt n chr
         where invertAt n orig = take n orig ++ [el] ++ drop (n+1) orig
@@ -64,7 +66,7 @@ mutateChromosome chr = do
 
 crossingover::Chromosome->Chromosome->GenRand (Chromosome, Chromosome)
 crossingover a b = do
-        n <- uniform [0..length a -1]
+        n <- uniform [0,3..length a -1]
         return $ f n
         where f n = (take n a ++ drop n b, take n b ++ drop n a)
 
@@ -73,12 +75,12 @@ runEvol opts@(EvOptions{..}) n pop =
    if (fitness (best pop) >= targetFitness)||(n>=maxGeneration)
    then return (n,pop)
    else do
-        -- | when(n `mod` 10 == 0) $ print $ show n ++ "-" ++ show pop
+        when(n `mod` 10 == 0) $ print $ show n ++ "-" ++ show pop
         rng <- newStdGen
         newPop <- evalRandT (nextPopulation opts pop) rng
         runEvol opts (n+1) newPop
    where best p = snd . maximumBy (compare `on` fst) $ zip (fitness <$> p) p
-   
+
 
 initEvol::EvOptions->IO (Int,Population)
 initEvol opts = do 
--- src/Main.hs
+++ src/Main.hs
@@ -4,21 +4,25 @@ import Genes;
 import Data.List
 import Data.List.Split
 import Data.Function(on)
-import Data.Maybe
-
+--import Debug.Trace
+                                                                    --Изменения - мутация длинной в город, кроссинговером режем длинной в город.
 fit::Fitness
 fit chr = sum $ map fromIntegral chr
 
 cityfit:: Int -> String -> Fitness
 cityfit cityNum citymap chr = do ((sum ( map read mapfit :: [Double])) + allq::Double)
-                 where mapfit = map (\city -> (lcitymap !! (fst city)) !! (snd city) ) roadList
+                 where mapfit = map (\(a,b) -> (lcitymap !! a !!b)) roadList
                        roadList = zip decChr $ tail decChr
-                       decChr = map bin2dec $ splitEvery chrLength chr
+                       decChr = map bin2dec $ chunksOf chrLength chr
                        lcitymap = map (\x -> words x) $ lines citymap
-                       chrLength = ceiling $ logBase 2.0 $ fromIntegral cityNum  
+                       chrLength = ceiling lo
+                       lo::Double
+                       lo = logBase 2.0 $ fromIntegral cityNum
                        allq = if ((length decChr) == (length $ nub decChr)) 
                               then 0.0
-                              else -1000.0
+                              else -5000.0
+
+
 
 bin2dec :: [Int] -> Int
 bin2dec l = sum $ map (2^) $ findIndices (==1) $ reverse l
@@ -30,27 +34,28 @@ opts= EvOptions{populationSize = 42,
                 fitness = fit,
                 targetFitness = 0,
                 mutationChance = 0.3,
-                elitePart = 0.1}
+                elitePart = 0.3}
 
 main::IO()
 main = do
    cnum <- getLine
    let cityNum = read cnum :: Int
-   let chrLength = ceiling $ logBase 2.0 $ fromIntegral cityNum
-   citymap <- readFile "citymap.txt"   
+   let lo = logBase 2.0 $ fromIntegral cityNum :: Double
+   let chrLength = ceiling lo
+   citymap <- readFile "citymap.txt"
 
-   let lcitymap = map (\x -> words x) $ lines citymap
-   let z = [1,0,0,0,1,1] :: [Int]
+--   let lcitymap = map (\x -> words x) $ lines citymap
+--   let z = [1,0,0,0,1,1] :: [Int]
    
    let l = cityfit cityNum citymap
 
-   let cityOpts = EvOptions 42 (chrLength*cityNum) 100 l 0 0.3 0.1   
+   let cityOpts = EvOptions 200 (chrLength*cityNum) 2000 l 0 0.3 0.1  
    (n, result) <- initEvol cityOpts
   
    print $ show n ++ "-" ++ show result 
    print $ snd . maximumBy (compare `on` fst) $ zip (map l result) result
    let bestChr = snd . maximumBy (compare `on` fst) $ zip (map l result) result
-   let decChr = map bin2dec $ splitEvery chrLength bestChr
+   let decChr = map bin2dec $ chunksOf chrLength bestChr
    let roadList = zip decChr $ tail decChr 
    print roadList
    print $ l bestChr
