cb3dface992d5bdab20559160cdfe81f86de902d
Anatoly <nazgull09@gmail.com>
2015-04-08 08:10:28 -0700

    extended graph

--- src/Genes.hs
+++ src/Genes.hs
@@ -7,6 +7,8 @@ import Data.Functor;
 import Data.Function(on)
 import Data.List(sortBy);
 import Data.Foldable(maximumBy);
+import Debug.Trace
+import Control.Monad.IO.Class
 
 type Chromosome = [Int]
 type Population = [Chromosome]
@@ -15,6 +17,7 @@ type GenRand = RandT StdGen IO
 data EvOptions = EvOptions{
                 populationSize::Int,
                 individLength::Int,
+                cityn::Int,
                 maxGeneration::Int,
                 fitness::Fitness,
                 targetFitness::Double,
@@ -28,14 +31,14 @@ randChoice :: Rational -> GenRand a -> GenRand a -> GenRand a
 randChoice chance th els = join (fromList [(th, chance), (els, 1 - chance)])        --fromList - часть рандома, выдает рандом из списка по вероятности
 
 nextPopulation::EvOptions->Population->GenRand Population
-nextPopulation EvOptions {..} pop = do
+nextPopulation opts@(EvOptions {..}) pop = do
         newPop' <-liftM concat $ replicateM (nonElite `div` 2) $ do
             a1 <- takeChr
             b1 <- takeChr
-            (a2, b2) <- crossingover a1 b1
-            a3 <- applyMutation a2
-            b3 <- applyMutation b2
-            return [a3, b3]
+            (a2, b2) <- traceShow ("Crossover", a1, b1) $ crossingover a1 b1
+            a3 <- traceShow ("mutate", a2) $ applyMutation a2
+            b3 <- traceShow ("mutate", b2) $ applyMutation b2
+            return $ traceShow (a3, b3) $ [a3, b3]
         let newPop = elite ++ newPop'
         return $ if length newPop <= length pop then newPop else tail newPop
         where   fits = toRational <$> fitness  <$> pop                              --map fitness pop
@@ -43,48 +46,48 @@ nextPopulation EvOptions {..} pop = do
                 chances = zip pop ((/maxfit) <$> fits)
                 takeChr = fromList chances
                 -- с некоторым шансом либо смутировать, либо просто венуть хромосому с
-                applyMutation c = randChoice (toRational mutationChance) (mutateChromosome c) (return c) 
+                applyMutation c = randChoice (toRational mutationChance) (mutateChromosome c opts) (traceShow ("no mutation", c) $ return c) 
                 sortedPop = snd $ unzip $ sortBy (compare `on` fst) $ zip (fitness <$> pop) pop 
                 -- другой подход к сортировке, иквабл для хромосомы
                 elite = take (ceiling $ fromIntegral (length pop) * elitePart) sortedPop
                 nonElite = length pop - length elite
 
-randChromosome::Int->GenRand Chromosome
-
-randChromosome n = replicateM n randBool
-        where randBool = uniform [0, 1]
+randChromosome::Int->Int->GenRand Chromosome
+randChromosome n cityn = ([0] ++) <$> replicateM (n-1) randCity              --Добавить контакт [0]
+        where randCity = uniform [-1..cityn-1]
         
-randPopulation::Int->Int->GenRand Population
-randPopulation n m = replicateM n $ randChromosome m
+randPopulation::Int->Int->Int->GenRand Population
+randPopulation n m cityn = replicateM n $ randChromosome m cityn
 
-mutateChromosome::Chromosome->GenRand Chromosome
-mutateChromosome chr = do                                --заменяем н-ый элемент инверсией
-        n <- uniform [0..length chr - 1]
-        return $ invertAt n chr
-        where invertAt n orig = take n orig ++ [el] ++ drop (n+1) orig
-                where el = if orig !! n == 0 then 1 :: Int else 0
-
+mutateChromosome::Chromosome->EvOptions->GenRand Chromosome
+mutateChromosome chr opts = traceShow ("Mutating ", chr) $ do                              --заменяем н-ый элемент инверсией
+        n <- uniform [0..length chr - 1] --ERROR
+        let arr = traceShow ("taking n ", n) $ filter (\i -> if n/=0 then (i/=chr!!n && i/=chr!!(n-1))  else i/=chr!!n ) [0..cityn opts-1]
+        newA <- traceShow ("taking from arr ", arr) $ uniform arr
+        liftIO $ print newA
+        let res = traceShow ("newA ", newA) $ take n chr ++ [newA] ++ drop (n+1) chr 
+        return $ traceShow ("res ", res) $ res
+        
 crossingover::Chromosome->Chromosome->GenRand (Chromosome, Chromosome)
 crossingover a b = do
-        n <- uniform [0,3..length a -1]
+        n <- uniform [0..length a -1]
         return $ f n
         where f n = (take n a ++ drop n b, take n b ++ drop n a)
 
-runEvol::EvOptions->Int->Population->IO (Int,Population)
+runEvol::EvOptions->Int->Population->IO (Int,Population,Chromosome)
 runEvol opts@(EvOptions{..}) n pop =
-   if (fitness (best pop) >= targetFitness)||(n>=maxGeneration)
-   then return (n,pop)
+   if (fitness best >= targetFitness)||(n>=maxGeneration)
+   then return (n,pop, best)
    else do
-        when(n `mod` 10 == 0) $ print $ show n ++ "-" ++ show pop
+        -- when(n `mod` 10 == 0) $ print $ show n ++ "-" ++ show pop
         rng <- newStdGen
         newPop <- evalRandT (nextPopulation opts pop) rng
         runEvol opts (n+1) newPop
-   where best p = snd . maximumBy (compare `on` fst) $ zip (fitness <$> p) p
+   where best = snd . maximumBy (compare `on` fst) $ zip (fitness <$> pop) pop
 
 
-initEvol::EvOptions->IO (Int,Population)
+initEvol::EvOptions->IO (Int,Population,Chromosome)
 initEvol opts = do 
    rng <- newStdGen
-   initPopulation <- evalRandT (randPopulation (populationSize opts) (individLength opts)) rng
-   result <- runEvol opts 0 initPopulation
-   return result
+   initPopulation <- evalRandT (randPopulation (populationSize opts) (individLength opts) (cityn opts)) rng
+   runEvol opts 0 initPopulation
--- src/Main.hs
+++ src/Main.hs
@@ -2,26 +2,25 @@ module Main where
 
 import Genes;
 import Data.List
-import Data.List.Split
-import Data.Function(on)
---import Debug.Trace
-                                                                    --Изменения - мутация длинной в город, кроссинговером режем длинной в город.
+--import Data.Function(on)
+import Debug.Trace
+import Control.Monad.Random
+import Control.Monad                                         --Изменения - мутация длинной в город, кроссинговером режем длинной в город.
 fit::Fitness
 fit chr = sum $ map fromIntegral chr
 
-cityfit:: Int -> String -> Fitness
-cityfit cityNum citymap chr = do ((sum ( map read mapfit :: [Double])) + allq::Double)
-                 where mapfit = map (\(a,b) -> (lcitymap !! a !!b)) roadList
-                       roadList = zip decChr $ tail decChr
-                       decChr = map bin2dec $ chunksOf chrLength chr
-                       lcitymap = map (\x -> words x) $ lines citymap
-                       chrLength = ceiling lo
-                       lo::Double
-                       lo = logBase 2.0 $ fromIntegral cityNum
-                       allq = if ((length decChr) == (length $ nub decChr)) 
-                              then 0.0
-                              else -5000.0
-
+cityfit:: [[Int]] -> Fitness
+cityfit intCityMap = foo 
+   where
+       foo :: Fitness 
+       foo chr = traceShow (chr, chrPure) $ if length chrPure == 0 
+                    then -9001.0 
+                    else traceShow bonus bonus + traceShow cost cost
+         where chrPure = filter (/=(-1)) chr
+               bonus::Double
+               bonus = 100.0 * (fromIntegral $ length chr) / (fromIntegral $ length chrPure)
+               cost :: Double
+               cost = fromIntegral (sum $ map (\(a,b) -> (intCityMap!!a)!!b) $ zip chrPure $ tail chrPure) 
 
 
 bin2dec :: [Int] -> Int
@@ -29,33 +28,35 @@ bin2dec l = sum $ map (2^) $ findIndices (==1) $ reverse l
 
 opts::EvOptions
 opts= EvOptions{populationSize = 42,
-                individLength = 10,
-                maxGeneration = 100,
+                individLength = 30,
+                cityn = 16,
+                maxGeneration = 30,
                 fitness = fit,
-                targetFitness = 0,
+                targetFitness = 9000,
                 mutationChance = 0.3,
                 elitePart = 0.3}
 
 main::IO()
 main = do
-   cnum <- getLine
-   let cityNum = read cnum :: Int
-   let lo = logBase 2.0 $ fromIntegral cityNum :: Double
-   let chrLength = ceiling lo
-   citymap <- readFile "citymap.txt"
-
---   let lcitymap = map (\x -> words x) $ lines citymap
---   let z = [1,0,0,0,1,1] :: [Int]
-   
-   let l = cityfit cityNum citymap
-
-   let cityOpts = EvOptions 200 (chrLength*cityNum) 2000 l 0 0.5 0.1
-   (n, result) <- initEvol cityOpts
-  
-   print $ show n ++ "-" ++ show result 
-   print $ snd . maximumBy (compare `on` fst) $ zip (map l result) result
-   let bestChr = snd . maximumBy (compare `on` fst) $ zip (map l result) result
-   let decChr = map bin2dec $ chunksOf chrLength bestChr
-   let roadList = zip decChr $ tail decChr 
-   print roadList
-   print $ l bestChr
+    cityMap <- readFile "citymap.txt"
+    let lcityMap = map (\x -> words x) $ lines cityMap
+    let intCityMap = map (\row -> map (\way -> read way ::Int) row) lcityMap
+    let cityOpts= EvOptions{populationSize = 42,
+                individLength = 10,
+                cityn = 3,
+                maxGeneration = 50,
+                fitness = cityfit intCityMap,
+                targetFitness = 9000,
+                mutationChance = 0.3,
+                elitePart = 0.3}
+    {-
+    print $ cityfit intCityMap [0,-1,-1,2,1,0,1,0,2,2]
+    forever $ do
+        rng <- newStdGen
+        print =<< evalRandT (mutateChromosome [0,-1,-1,2,1,0,1,0,2,2] cityOpts) rng 
+    -}
+    
+    (n, result, best) <- initEvol cityOpts
+    print $ "Best one: " ++ show best
+    print $ "Best fit: " ++ (show $ cityfit intCityMap best)
+    print $ show n ++ "-" ++ show result
